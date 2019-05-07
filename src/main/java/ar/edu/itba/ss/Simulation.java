4f34232bd9d0ffbd82a7bc5ba37ba5ce6e60f6d3
Martin Grabina <mgrabina@itba.edu.ar>
2019-05-07 09:54:25 -0300

    parameters

--- src/main/java/ar/edu/itba/ss/Simulation.java
+++ src/main/java/ar/edu/itba/ss/Simulation.java
@@ -1,9 +1,14 @@
 package ar.edu.itba.ss;
 
+import ar.edu.itba.ss.GranularMedia.GranularMediaForce;
+import ar.edu.itba.ss.Integrators.GearPredictor;
+import ar.edu.itba.ss.Integrators.Integrator;
+
 public class Simulation
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Double dt = 0.1;
+        Integrator integrator = new GearPredictor(dt, new GranularMediaForce());
     }
 }
--- src/main/java/ar/edu/itba/ss/io/Input.java
+++ src/main/java/ar/edu/itba/ss/io/Input.java
@@ -8,39 +8,21 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Input {
-
-
-    private Long ParticlesQuantity;
     private List<Particle> particles;
-    private static final Double defaultVelocity = 10.0;
-    public static Double time = 35.0;
-    private static final Double dt = 0.1;
-    private static final Double ParticleRadio = 0.0;
-    private static final Double ParticleMass = 0.1;
-    private int cellSideQuantity;
-    private double interactionRadio;
-
-    // Default Parameters
-    private Double Rm = 1.0;
-    private Double epsilon = 2.0;
-    private Double m = 0.1;
-    private Double r = 5.0;
-    private Integer boxWidth = 400;
-    private Integer boxHeight = 200;
-    private Integer middleBoxWidth = boxWidth/2;
-    private Integer orificeLength = 10;
-
-    // Default Parameters for Oscillation
-    private final Double A = 1.0;       // TODO: Put real value
-    private final Double K = 10000.0;   // In N/m
-    private final Double y = 100.0;    // In kg/s
-    private final Double Tf = 5.0;     // In s
-    private final Double M = 70.0;     // In Kilogrames
-    private final Double initialX = 1.0;          // In m
-    private final Double initialV = -A*y/(2*M);   // In m/s
-    private final Double initialA = (-K*initialX-y*initialV)/M;   // In m/s
-    private static Double endTime = 5.0; // In s
-
+    private int particlesQuantity;
+    private final double minL = 1d;   
+    private final double maxL = 1.5;   
+    private final double minW = 0.3;   
+    private final double maxW = 0.4;   
+    private final double minD = 0.15;   
+    private final double maxD = 0.25;   
+    private final boolean contornConditions = true; //Defined; Only on after the opening
+    private final double minRadio = 0.02;
+    private final double maxRadio = 0.03;
+    private final double Kn = Math.pow(10, 5);
+    private final double Kt = 2d*Kn;
+    private final double y = 70d;
+    private final double mass = 0.01;
 
     public Input(){
 
@@ -51,153 +33,103 @@ public class Input {
      */
     public Input(Long quantity, double dt){
         System.out.print("[Generating Input... ");
-        this.interactionRadio = r;
-        this.cellSideQuantity = (int) Math.ceil(boxWidth/interactionRadio);
-        this.ParticlesQuantity = quantity;
-        this.particles = new ArrayList<>();
-
-        for (int p = 0 ; p < ParticlesQuantity ; p++ ){
-            Double x,y,vX,vY;
-            do{
-                x =  ThreadLocalRandom.current().nextDouble(r, boxHeight-r);
-                y =  ThreadLocalRandom.current().nextDouble(r, boxHeight-r);
-                double random = 2 * Math.PI * Math.random();
-                vX = defaultVelocity*Math.cos(random);
-                vY = defaultVelocity*Math.sin(random);
-            }while(!noOverlapParticle(x,y));
-            this.particles.add(new Particle(
-                    ParticleRadio,
-                    ParticleMass,
-                    x,
-                    y,
-                    vX,
-                    vY,
-                    dt
-            ));
-        }
+//        this.interactionRadio = r;
+//        this.cellSideQuantity = (int) Math.ceil(boxWidth/interactionRadio);
+//        this.ParticlesQuantity = quantity;
+//        this.particles = new ArrayList<>();
+//
+//        for (int p = 0 ; p < ParticlesQuantity ; p++ ){
+//            Double x,y,vX,vY;
+//            do{
+//                x =  ThreadLocalRandom.current().nextDouble(r, boxHeight-r);
+//                y =  ThreadLocalRandom.current().nextDouble(r, boxHeight-r);
+//                double random = 2 * Math.PI * Math.random();
+//                vX = defaultVelocity*Math.cos(random);
+//                vY = defaultVelocity*Math.sin(random);
+//            }while(!noOverlapParticle(x,y));
+//            this.particles.add(new Particle(
+//                    ParticleRadio,
+//                    ParticleMass,
+//                    x,
+//                    y,
+//                    vX,
+//                    vY,
+//                    dt
+//            ));
+//        }
         System.out.println("Done.]");
     }
 
     private boolean noOverlapParticle(Double x, Double y){
+
+        // TODO: Check if need to add the current particle radio or some minimum value
+
         if (particles.size() == 0) return true;
         for (Particle particle : particles){
-                if ( (Math.pow(particle.getX() - x, 2) + Math.pow(particle.getY() - y, 2)) <= Math.pow(particle.getRadius() + ParticleRadio + r, 2)){
+                if ( (Math.pow(particle.getX() - x, 2) + Math.pow(particle.getY() - y, 2)) <= Math.pow(particle.getRadius(), 2)){
                     return false;
             }
         }
         return true;
     }
 
-
-
     public List<Particle> getParticles() {
         return particles;
     }
 
-    public int getSystemSideLength() {
-        return boxWidth>boxHeight?boxWidth:boxHeight;
+    public int getParticlesQuantity() {
+        return particlesQuantity;
     }
 
-    public int getCellSideQuantity() {
-        return cellSideQuantity;
+    public double getMinL() {
+        return minL;
     }
 
-    public void setCellSideQuantity(int cellSideQuantity) {
-        this.cellSideQuantity = cellSideQuantity;
+    public double getMaxL() {
+        return maxL;
     }
 
-    public double getInteractionRadio() {
-        return interactionRadio;
+    public double getMinW() {
+        return minW;
     }
 
-    public Long getParticlesQuantity() {
-        return ParticlesQuantity;
+    public double getMaxW() {
+        return maxW;
     }
 
-    public static Double getDefaultVelocity() {
-        return defaultVelocity;
+    public double getMinD() {
+        return minD;
     }
 
-    public static Double getTime() {
-        return time;
+    public double getMaxD() {
+        return maxD;
     }
 
-    public static Double getDt() {
-        return dt;
+    public boolean isContornConditions() {
+        return contornConditions;
     }
 
-    public static Double getParticleRadio() {
-        return ParticleRadio;
+    public double getMinRadio() {
+        return minRadio;
     }
 
-    public static Double getParticleMass() {
-        return ParticleMass;
+    public double getMaxRadio() {
+        return maxRadio;
     }
 
-    public Double getRm() {
-        return Rm;
+    public double getKn() {
+        return Kn;
     }
 
-    public Double getEpsilon() {
-        return epsilon;
+    public double getKt() {
+        return Kt;
     }
 
-    public Double getM() {
-        return m;
-    }
-
-    public Double getMass() {
-        return M;
-    }
-
-    public Double getR() {
-        return r;
-    }
-
-    public Integer getBoxWidth() {
-        return boxWidth;
-    }
-
-    public Integer getBoxHeight() {
-        return boxHeight;
-    }
-
-    public Integer getMiddleBoxWidth() {
-        return middleBoxWidth;
-    }
-
-    public Integer getOrificeLength() {
-        return orificeLength;
-    }
-
-    public Double getA() {
-        return A;
-    }
-
-    public Double getInitialV() {
-        return initialV;
-    }
-
-    public static Double getEndTime() {
-        return endTime;
-    }
-
-    public Double getK() {
-        return K;
-    }
-
-    public Double getY() {
+    public double getY() {
         return y;
     }
 
-    public Double getTf() {
-        return Tf;
-    }
-
-    public Double getInitialX() {
-        return initialX;
-    }
-    public Double getInitialA() {
-        return initialA;
+    public double getMass() {
+        return mass;
     }
 }
